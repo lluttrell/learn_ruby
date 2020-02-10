bee6c59c8d8480607b68bae92f75ae618ff06de4
Richard Williams <richard.lluttrell@gmail.com>
2020-02-10 12:12:40 -0700

    finished exercise 2

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+def add(num1,num2)
+    num1 + num2
+end
+
+def subtract(num1,num2)
+    num1 - num2
+end
+
+def sum array
+    output = 0
+    array.each {|value| output += value}
+    output
+end 
+
+def multiply (*numbers)
+    output = 1
+    numbers.each {|value| output *= value}
+    output
+end
+
+def power (num1, num2)
+    num1 ** num2 
+end
+
+def factorial num
+    output = 1
+    while num > 0
+        output *= num
+        num -= 1
+    end
+    output
+end
