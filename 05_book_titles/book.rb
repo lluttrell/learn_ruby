6a23d4c9837a7d381651c03e4754e0a95c3da36f
Richard Williams <richard.lluttrell@gmail.com>
2020-02-11 14:22:06 -0700

    completed exercise 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+    @title
+
+    def title=(title)
+        title_words = title.split
+        title = ""
+        title_words.each_with_index do |w,i|
+            conjunctions = ['the','a','and','in','of','an']
+            if conjunctions.include?(w) and i != 0
+                title += w + " "
+            else
+                title += w.capitalize + " "
+            end
+        end
+        @title = title[0..-2]
+    end
+
+    def title
+        @title
+    end
 end
