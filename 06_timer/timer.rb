936211a9d6a416893f8792a0cdd9d35cdbaa1864
Richard Williams <richard.lluttrell@gmail.com>
2020-02-11 14:54:19 -0700

    finished assignment 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(seconds)
+    @seconds = seconds
+  end
+
+  def time_string
+    hours = seconds / 3600
+    minutes = (seconds - hours * 3600) / 60
+    secs = seconds - hours * 3600 - minutes * 60
+    "#{"%02d" %hours}:#{"%02d" %minutes}:#{"%02d" %secs}"
+  end 
+
 end
