7426ca03ef2c1cda10d9215c3556a0cb3508a63e
Richard Williams <richard.lluttrell@gmail.com>
2020-02-11 13:48:49 -0700

    working except qu

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
 #write your code here
+
+def translate_single_word word
+    new_word = ""
+    first_vowel = /[aeiouy]/ =~ word
+    if first_vowel == 0
+        new_word += word + "ay" 
+    else
+        new_word = word[first_vowel..-1] + word[0..first_vowel-1] + "ay"  
+    end    
+end
+        
+def translate word
+    new_sentence = ""
+    words = word.split
+    words.each do |w| 
+        new_sentence += translate_single_word(w) + " "
+    end
+    new_sentence = new_sentence[0..-2]
+end  
